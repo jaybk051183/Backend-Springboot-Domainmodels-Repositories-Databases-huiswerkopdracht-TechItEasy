7387fc343abc915f395302b1dd8e3d5988a330ea
jaybk051183 <kailashbhaggoe@outlook.com>
2023-02-28 14:16:20 +0100

    project initiation

---
+++ src/main/java/com/example/domainmodelopdrachtspringbootles11/DomainModelOpdrachtSpringbootLes11Application.java
@@ -0,0 +1,13 @@
+package com.example.domainmodelopdrachtspringbootles11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainModelOpdrachtSpringbootLes11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainModelOpdrachtSpringbootLes11Application.class, args);
+    }
+
+}

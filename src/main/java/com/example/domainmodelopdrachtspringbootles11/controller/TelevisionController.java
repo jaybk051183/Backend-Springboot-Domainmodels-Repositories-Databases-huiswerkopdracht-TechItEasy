c6082ecc6c8e11ed6aed2438cdc9ab8a51263a36
jaybk051183 <kailashbhaggoe@outlook.com>
2023-03-24 17:55:25 +0100

    corrections

--- src/main/java/com/example/domainmodelopdrachtspringbootles11/controller/TelevisionController.java
+++ src/main/java/com/example/domainmodelopdrachtspringbootles11/controller/TelevisionController.java
@@ -1,67 +1,123 @@
 package com.example.domainmodelopdrachtspringbootles11.controller;
 
 import com.example.domainmodelopdrachtspringbootles11.exceptions.RecordNotFoundException;
+import com.example.domainmodelopdrachtspringbootles11.model.Television;
+import com.example.domainmodelopdrachtspringbootles11.repository.TelevisionRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TelevisionController {
-    ArrayList<String >televisionDataBase = new ArrayList<>();
+    // Vorige week maakten we nog gebruik van een List<String>, nu gebruiken we de repository met een echte database.
+    // We injecteren de repository hier via de constructor, maar je mag ook @Autowired gebruiken.
+    private final TelevisionRepository televisionRepository;
 
-    @GetMapping("television")
-    public ResponseEntity<ArrayList<String>> getAllTelevisions() {
-        televisionDataBase.add("Sony Bravia");
-        televisionDataBase.add("Samsung QLED");
-        televisionDataBase.add("LG OLED");
-        return new ResponseEntity<>(televisionDataBase, HttpStatus.OK);
+    // Constructor injection
+    public TelevisionController(TelevisionRepository televisionRepository) {
+        this.televisionRepository = televisionRepository;
     }
 
-    @GetMapping("television/{id}")
-    public ResponseEntity<String> getTelevisionById(@PathVariable int id) {
+    // We hebben hier de RequestParam "brand" toegevoegd om te laten zien hoe dat werkt.
+    // Als de gebruiker een brand invult, dan zoeken we op brand, anders returnen we alle televisions.
+    @GetMapping("/televisions")
+    public ResponseEntity<List<Television>> getAllTelevisions(@RequestParam(value = "brand", required = false) String brand) {
+        List<Television> televisions;
+        if (brand == null) {
+            // Vul de televisions lijst met alle televisions
+            televisions = televisionRepository.findAll();
+
+        } else {
+            // Vul de televisions lijst met alle television die een bepaald brand hebben
+            televisions = televisionRepository.findAllTelevisionsByBrandEqualsIgnoreCase(brand);
+        }
+        // Return de televisions lijst en een 200 status
+        return ResponseEntity.ok().body(televisions);
+    }
+
+    // Zo ja, dan haal je de Television uit de Optional met "optionalTelevision.get()" en return je dat in responsestatus.ok, zo nee, dan return je een ResponseEntity.badRequest() of gooi je een RecordNotFoundException op.
+    @GetMapping("televisions/{id}")
+    public ResponseEntity<Television> getTelevisionById(@PathVariable Long id) {
         String television;
-        if (id != 1 && id != 2 && id != 3) {
-            throw new RecordNotFoundException("Opgegeven ID kan niet gevonden worden");
+        //Repository call maken "repository.findById(id)".
+        Optional<Television> optionalTelevision = televisionRepository.findById(id);
+// Daar krijg je een Optional van terug. Vervolgens checken of er een Television in die Optional zit of niet, door optionalTelevision.isPresent() te checken.
+        if (optionalTelevision.isPresent()) {
+            // Zo ja, dan haal je de Television uit de Optional met "optionalTelevision.get()" en return je dat in responsestatus.ok, zo nee, dan return je een ResponseEntity.badRequest() of gooi je een RecordNotFoundException op.
+            Television television1 = optionalTelevision.get();
+            return new ResponseEntity<>(television1, HttpStatus.OK);
         } else {
-            if (id == 1) {
-                television = "Sony Bravia";
-            } else if (id == 2) {
-                television = "Samsung QLED";
+            throw new RecordNotFoundException("No television found with id: " + id);
+        }
+    }
+
+    // We geven hier een television mee in de parameter. Zorg dat je JSON object exact overeenkomt met het Television object.
+        @PostMapping("televisions")
+        public ResponseEntity<Television> addTelevision (@RequestBody Television television){
+            // Sla de nieuwe tv in de database op met de save-methode van de repository
+            Television returnTelevision = televisionRepository.save(television);
+            // Return de gemaakte television en een 201 status
+            return new ResponseEntity<>(returnTelevision, HttpStatus.CREATED);
+        }
+
+        @DeleteMapping("televisions/{id}")
+        public ResponseEntity<Object> deleteTelevision ( @PathVariable Long id) {
+            // Check eerst of the id bestaat in de database, verwijder dan de television met het opgegeven id uit de database.
+            if (televisionRepository.existsById(id)) {
+                televisionRepository.deleteById(id);
+                //Retourneer een response entity met status no content.
+                return new ResponseEntity<>("Television deleted succesfully",HttpStatus.NO_CONTENT);
             } else {
-                television= "LG OLED";
+                //Zo niet, gooi dan een exception
+                throw new RecordNotFoundException("Television doesn't exist");
             }
-            return new ResponseEntity<>(television, HttpStatus.OK);
+
         }
+
+        @PutMapping("/televisions/{id}")
+        public ResponseEntity<Television> updateTelevision (@PathVariable Long id, @RequestBody Television newTelevision)
+        {
+            // Haal de aan te passen tv uit de database met het gegeven id
+            Optional<Television> television1 = televisionRepository.findById((long) id);
+
+            // Als eerste checken we of de aan te passen tv wel in de database bestaat.
+            if (television1.isEmpty()) {
+
+                throw new RecordNotFoundException("No television found with id: " + id);
+
+            } else {
+                // Verander alle waardes van de television uit de database naar de waardes van de television uit de parameters.
+                // Behalve de id. Als je de id veranderd, dan wordt er een nieuw object gemaakt in de database.
+                Television television2 = television1.get();
+                television2.setAmbiLight(newTelevision.getAmbiLight());
+                television2.setAvailableSize(newTelevision.getAvailableSize());
+                television2.setAmbiLight(newTelevision.getAmbiLight());
+                television2.setBluetooth(newTelevision.getBluetooth());
+                television2.setBrand(newTelevision.getBrand());
+                television2.setHdr(newTelevision.getHdr());
+                television2.setName(newTelevision.getName());
+                television2.setOriginalStock(newTelevision.getOriginalStock());
+                television2.setPrice(newTelevision.getPrice());
+                television2.setRefreshRate(newTelevision.getRefreshRate());
+                television2.setScreenQuality(newTelevision.getScreenQuality());
+                television2.setScreenType(newTelevision.getScreenType());
+                television2.setSmartTv(newTelevision.getSmartTv());
+                television2.setSold(newTelevision.getSold());
+                television2.setType(newTelevision.getType());
+                television2.setVoiceControl(newTelevision.getVoiceControl());
+                television2.setWifi(newTelevision.getWifi());
+                // Sla de gewijzigde waarden op in de database onder dezelfde id. Dit moet je niet vergeten.
+                Television returnTelevision = televisionRepository.save(television2);
+                // Return de nieuwe versie van deze tv en een 200 code
+                return ResponseEntity.ok().body(returnTelevision);
+            }
+
+        }
+
     }
 
-    @PutMapping("television/{id}")
-    public ResponseEntity<String> updateTelevision(@PathVariable int id, @RequestBody String television) {
-        if (id >= televisionDataBase.size() || id < 0) {
-            return new ResponseEntity<>("Invalid index", HttpStatus.BAD_REQUEST);
-        } else {
-            televisionDataBase.set(id, television);
-            return new ResponseEntity<>("Television corrected successfully", HttpStatus.OK);
-        }
-    }
 
-    @PostMapping("television")
-    public ResponseEntity<String> addTelevision(@RequestBody String television) {
-            televisionDataBase.add(television);
-            return new ResponseEntity<>("Television added successfully", HttpStatus.CREATED);
-        }
 
-    @DeleteMapping("television/{id}")
-    public ResponseEntity<String> deleteTelevision(@PathVariable int id) {
-        if (id >= televisionDataBase.size() || id < 0) {
-            return new ResponseEntity<>("Invalid index", HttpStatus.BAD_REQUEST);
-        }
-        else {
-            televisionDataBase.set(id, null);
-            return new ResponseEntity<>("Television deleted successfully", HttpStatus.OK);
-        }
-
-    }
-
-}
--- src/main/java/com/example/domainmodelopdrachtspringbootles11/repository/TelevisionRepository.java
+++ src/main/java/com/example/domainmodelopdrachtspringbootles11/repository/TelevisionRepository.java
@@ -3,6 +3,9 @@ package com.example.domainmodelopdrachtspringbootles11.repository;
 import com.example.domainmodelopdrachtspringbootles11.model.Television;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TelevisionRepository extends JpaRepository<Television, Long> {
 
+    List<Television> findAllTelevisionsByBrandEqualsIgnoreCase(String brand);
 }
